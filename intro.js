c6bf853327c669b9e0f01121ee823115c0067389
Mohammed Uzer Qazi <108578286+uzer-28@users.noreply.github.com>
2023-11-22 19:27:20 +0530

    Add files via upload

---
+++ intro.js
@@ -0,0 +1,8 @@
+var name = "Mohammed Uzer"
+var city='Solapur'
+var year = 3
+var aim ='Full Stack Developer'
+
+console.log('Hello, myself '+name+'.\nI am from '+city+'. Currently I am in'+year+'rd year B.tech E&TC @ NBNSCOE.'+'\nApart from my core I am learning to become '+aim+' @ InfoStack Software &Research Center.')
+
+console.log(`Hello, myself ${name}.\nI am from ${city}. Currently I am in ${year}rd year B.tech E&TC @ NBNSCOE.\nApart from my core I am learning to become ${aim} @ InfoStack Software &Research Center.`)
